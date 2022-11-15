9e850c7718128fbad4327d37c016797640c7b44b
shashiKantMore7 <shri.more198@gmail.com>
2022-11-15 13:35:24 +0530

    first commit

---
+++ src/components/ItemList.js
@@ -0,0 +1,28 @@
+import Task from "./Task";
+
+const ItemList = ({
+  tasks,
+  handleDoubleClick,
+  setEditableId,
+  editableId,
+  handleEditing,
+  handleDelete,
+}) => {
+  return (
+    <div className="items-list">
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          task={task}
+          handleDoubleClick={handleDoubleClick}
+          handleEditing={handleEditing}
+          handleDelete={handleDelete}
+          setEditableId={setEditableId}
+          editableId={editableId}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default ItemList;
